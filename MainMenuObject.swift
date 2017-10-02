a80f2d3cea3f1473d9507e26b032fd04fe2f21d4
Suhib Alsisan <salsisan@fb.com>
2017-10-02 00:56:28 -0700

    Set starting window size and add new sample files with more text

---
+++ MainMenuObject.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuObject.swift
+//  difftool
+//
+//  Created by Suhib Alsisan on 9/23/17.
+//  Copyright © 2017 Yasser Elsayed. All rights reserved.
+//
+
+import Cocoa
+
+class MainMenuObject: NSObject {
+
+}
